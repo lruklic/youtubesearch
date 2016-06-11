e236f68e151c8a79429ae3bc0a3f18dad9b8f77a
Luka Ruklić <stijena@users.noreply.github.com>
2016-06-11 12:47:13 +0200

    Update videoListItem.js by Marin Marsic

--- app/components/videoListItem.js
+++ app/components/videoListItem.js
@@ -1,20 +1,33 @@
 import React from 'react';
-
+​
 const VideoItemList = ({video, onVideoSelect}) => {
+  const videoId = video.id.videoId;
+  const addUrl = "http://10.2.200.57:3000/playlists/" + videoId
+​
+  function postFunction() {
+    var http = new XMLHttpRequest();
+    http.open("POST", addUrl, true);
+    http.send();
+  }
+​
   return (
-    <li className="list-group-item" onClick={() => onVideoSelect(video)}>
+    <li className="list-group-item" >
       <div className="video-list media">
         <div className="media-left">
-          <img className="media-object" src={video.snippet.thumbnails.default.url} />
+          <img className="media-object" src={video.snippet.thumbnails.default.url} onClick={() => onVideoSelect(video)} />
         </div>
         <div className="media-body">
           <div className="media-heading">
             {video.snippet.title}
+          </div>
+          <div className="add-link" >
+            <a id="addLink" href="" onClick={postFunction} >add to playlist</a>
+​
           </div>
         </div>
       </div>
     </li>
   );
 }
-
+​
 export default VideoItemList;
