b3e8466957c2ed62fe911740e954eb9a3a877743
lruklic <ruklic.luka@gmail.com>
2016-06-11 11:28:40 +0200

    backend routes

--- server.babel.js
+++ server.babel.js
@@ -1,8 +1,20 @@
 import express from 'express';
 
+var playlists = require('./routes/playlists');
+
+var mongoose = require('mongoose');
+mongoose.connect('mongodb://127.0.0.1:27017/youtubedb');
+
 const app = express();
 
+app.use(function(req, res, next) {
+ res.header("Access-Control-Allow-Origin", "*");
+ res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+ next();
+});
+
 app.use('/', express.static('public'));
+app.use('/playlists', playlists);
 
 app.listen(process.env.PORT = 3000, () => {
   console.log('Express is listening on port: 3000');
