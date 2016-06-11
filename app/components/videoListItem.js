1803c361721eb9549009f6c857d4d270adc5499d
Luka Ruklić <stijena@users.noreply.github.com>
2016-06-11 12:49:53 +0200

    a

--- app/components/videoListItem.js
+++ app/components/videoListItem.js
@@ -1,33 +1,20 @@
 import React from 'react';
-​
+
 const VideoItemList = ({video, onVideoSelect}) => {
-  const videoId = video.id.videoId;
-  const addUrl = "http://10.2.200.57:3000/playlists/" + videoId
-​
-  function postFunction() {
-    var http = new XMLHttpRequest();
-    http.open("POST", addUrl, true);
-    http.send();
-  }
-​
   return (
-    <li className="list-group-item" >
+    <li className="list-group-item" onClick={() => onVideoSelect(video)}>
       <div className="video-list media">
         <div className="media-left">
-          <img className="media-object" src={video.snippet.thumbnails.default.url} onClick={() => onVideoSelect(video)} />
+          <img className="media-object" src={video.snippet.thumbnails.default.url} />
         </div>
         <div className="media-body">
           <div className="media-heading">
             {video.snippet.title}
-          </div>
-          <div className="add-link" >
-            <a id="addLink" href="" onClick={postFunction} >add to playlist</a>
-​
           </div>
         </div>
       </div>
     </li>
   );
 }
-​
+
 export default VideoItemList;
