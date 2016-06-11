bcac04c84ac384572a316f8895526fea63d89897
lruklic <ruklic.luka@gmail.com>
2016-06-11 12:33:16 +0200

    support for multiple playlists on backend

--- routes/playlists.js
+++ routes/playlists.js
@@ -16,7 +16,6 @@ router.post('/init', function(req, res, next) {
         	res.status(200).send('New playlist added.');
     	});
 
-    
 	});
 
 });
@@ -44,10 +43,63 @@ router.post('/:videoId', function(req, res, next) {
 	});
 });
 
+/* POST add new video to playlist */
+router.post('/:listId/:videoId', function(req, res, next) {
+
+	Playlist.findOne({ id: req.params.listId }).then(function(playlist) {
+  		if (!playlist) {	
+			var newPlaylist = new Playlist();
+			newPlaylist.id = req.params.listId;
+			newPlaylist.videos.push(req.params.videoId);
+	    	newPlaylist.save(function(err) {
+	       		if (err) {
+	        	    console.log('Problem occured');
+	        	}
+	        	res.status(200).send('New playlist added.');
+	    	});
+  		} else {
+    		playlist.videos.push(req.params.videoId);
+
+	    	playlist.save(function(err) {
+	      		if (err) {
+	        		console.log('Unable to add video to playlist.');
+	        		res.status(200).send('Unable to add video to playlist.');
+	      		}
+	      		else {
+	        		console.log('Video added to playlist.');
+	        		res.status(200).send('Video added to playlist.');
+	      		}
+	    	});
+		}
+	});
+});
+
 /* GET get favourites playlist */
 router.get('/', function(req, res, next) {
 
 	Playlist.findOne({ id: 0 }).then(function(playlist) {
+
+		var videos = [];
+		for (var i = 0; i < playlist.videos.length; i++) {
+			videos.push(playlist.videos[i]);
+		}
+
+		console.log(videos);
+
+  		res.setHeader('Content-Type', 'application/json');
+    	res.send(JSON.stringify(videos));
+	});
+});
+
+/* GET get favourites playlist */
+router.get('/:listId', function(req, res, next) {
+
+	var listId = 0;
+	if (req.params.listId) {
+		listId = req.params.listId;
+	}
+
+	Playlist.findOne({ id: req.params.listId }).then(function(playlist) {
 
 		var videos = [];
 		for (var i = 0; i < playlist.videos.length; i++) {
